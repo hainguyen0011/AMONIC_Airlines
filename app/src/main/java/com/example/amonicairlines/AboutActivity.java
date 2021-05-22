ae3d3e1aeeafb32f27f474d0cea3a98c0c2f50d3
Nguyen Ngoc Hai <hainguyen1120011@gmail.com>
2021-05-22 15:13:15 +0700

    Initial commit

---
+++ app/src/main/java/com/example/amonicairlines/AboutActivity.java
@@ -0,0 +1,21 @@
+package com.example.amonicairlines;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.view.View;
+import android.widget.ScrollView;
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+
+    public void onBackClick(View view){
+
+    }
+}
---
+++ app/src/main/java/com/example/amonicairlines/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.amonicairlines;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    public void onAboutClick(View view) {
+        Intent intent = new Intent(this, AboutActivity.class);
+        startActivity(intent);
+    }
+
+    public void onSearchClick(View view) {
+        Intent intent = new Intent(this, SearchActivity.class);
+        startActivity(intent);
+    }
+
+    public void onReserveClick(View view) {
+        Intent intent = new Intent(this, ReserveActivity.class);
+        startActivity(intent);
+    }
+
+    public void onAmenitiesClick(View view) {
+        Intent intent = new Intent(this, AmenitiesActivity.class);
+        startActivity(intent);
+    }
+}
